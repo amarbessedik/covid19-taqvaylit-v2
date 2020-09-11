b0781e32bd132ddf4766b2f3b237ccb51f95f089
amarbessedik <amar.bessedik@gmail.com>
2020-09-10 23:48:41 -0400

    Contient overlay toggling fixed

--- src/parts/Continent/Continent.js
+++ src/parts/Continent/Continent.js
@@ -1,20 +1,21 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styles from './Continent.module.css';
 import { prettyPrintStat } from "../../utils";
 import CloseIcon from "@material-ui/icons/Close";
 
 const Continent = ({ name, image, data }) => {
-  const [open, setOpen] = useState(false);
-  let overlay = document.getElementById('overlay');
-
-  const handleOverlay = () =>{
-      overlay.style.display = open ? 'none' : 'flex';
-      setOpen(!open);
+  let overlay = document.getElementById("overlay");
+  
+  const openOverlay = () =>{
+      overlay.style.display = 'flex';
   }
+  const closeOverlay = () => {
+    overlay.style.display = "none";
+  };
 
   return (
     <div className={styles.continent__wraper}>
-      <div onClick={handleOverlay} className={styles.continent}>
+      <div onClick={openOverlay} className={styles.continent}>
         <h4 className={styles.continent__title}>{name}</h4>
         <div className={styles.continent__image}>
           <img src={image} alt="continentMap" />
@@ -36,7 +37,7 @@ const Continent = ({ name, image, data }) => {
       </div>
       <div id="overlay" className={styles.continent__overlay}>
         <div className={styles.continent__overlay__data}>
-          <div onClick={handleOverlay} className={styles.overlay__close__icon__wraper}>
+          <div onClick={closeOverlay} className={styles.overlay__close__icon__wraper}>
             <CloseIcon className={styles.close__icon} />
           </div>
           <h1>overlay data</h1>
